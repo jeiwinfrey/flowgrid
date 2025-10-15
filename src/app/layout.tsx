daa63d38ceb7b45a9169e6657f40c48ccd4288b6
jeiwinfrey <jeiwinfreyulep12@gmail.com>
2025-10-15 10:46:56 +0800

    feat: integrate Toaster component for notifications in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -26,6 +27,7 @@ export default function RootLayout({
         className={`${outfit.variable} antialiased`}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   );
