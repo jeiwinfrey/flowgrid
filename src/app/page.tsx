aebffe5876379d875c634f6aa793e6c8905ec034
jeiwinfrey <jeiwinfreyulep12@gmail.com>
2025-10-16 04:19:37 +0800

    refactor: enhance layout styles for Home component with overflow handling and shrink properties

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import { ResizableBody } from "@/components/forms/resizeable-body"
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col">
+    <div className="h-screen flex flex-col overflow-hidden">
 
       {/* Header */}
-      <div className="flex items-center justify-between p-2">
+      <div className="flex items-center justify-between p-2 shrink-0">
         <div className="flex items-center gap-2 h-6">
           <img src="supabase-logo-icon.svg" alt="FlowGrid" width={16} height={16} /> 
           <Separator orientation="vertical"/>
@@ -15,15 +15,15 @@ export default function Home() {
         </div>
         <SupportDialog />
       </div>
-      <Separator />
+      <Separator className="shrink-0" />
       
       {/* Main body */}
-      <div className="flex-1">
+      <div className="flex-1 min-h-0">
         <ResizableBody />
       </div>
       
       {/* Footer */}
-      <div>
+      <div className="shrink-0">
         {/* Footer content goes here */}
       </div>
     </div>
